00f7ae25e564b8a258cf4c0fc460592f708a8072
Agostina <agoszingale30@gmail.com>
2025-05-18 22:32:00 -0300

    agrega bmi.c para imc

---
+++ bmi.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+    float peso, altura, imc;
+
+    printf("Ingrese su peso en kg: ");
+    scanf("%f", &peso);
+    printf("Ingrese su altura en metros: ");
+    scanf("%f", &altura);
+
+    imc= peso/(altura*altura);
+
+    printf("\nSu índice de masa corporal es: %.2f\n\n",imc);
+    printf("Índice\t\t| Condición\n");
+    printf("---------------------------\n");
+    printf("<18.5\t\t| Bajo peso\n");
+    printf("18.5 - 24.9\t| Normal\n");
+    printf("25.0 - 29.9\t| Sobrepeso\n");
+    printf(">=30\t\t| Obesidad\n");
+
+    return 0;
+}
